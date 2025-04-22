744eee7bf3d500f2f68eb17f2429c85111246d42
amantha6 <amantha@binghamton.edu>
2025-04-22 10:39:26 -0400

    Video 2 views, texts and styles

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // div component to wrap other elements can style diff components
+    <View style={styles.container}> 
+      {/* can nest views within views */}
+      <View style={styles.header}>
+        <Text style={styles.boldText}> Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text> Hi this is a sample body text</Text>
+        <Text> Hi this is a sample body text</Text>
+        <Text style={styles.boldText}> Hi this is a sample body text</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +25,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20,
+  },
+  boldText:{
+    fontWeight:'bold',
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20,
+    fontWeight:'bold',
+  }
 });
