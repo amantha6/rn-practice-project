be4ea4a342d4aab2b8da022777768e31954d0a5f
amantha6 <amantha@binghamton.edu>
2025-04-22 12:30:53 -0400

    Video 4 Using state

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[name, setName]=useState('Aditi'); //name variable here will be given the value of Aditi
+  const[person,setPerson]=useState({name:'Aarushi', age:23});
+  //in future if we want to update the value then we can use this function to do that
+  //create a function here externally
+  const clickHandler=()=>{
+    setName('Adhira');
+    setPerson({name: 'Tripti', age:26});
+  }
   return (
     // div component to wrap other elements can style diff components
     <View style={styles.container}> 
       {/* can nest views within views */}
       <View style={styles.header}>
-        <Text style={styles.boldText}> Hello World!</Text>
+        <Text style={styles.boldText}> My name is {name}</Text>
+        <Text> Her name is {person.name} and her age is {person.age}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='update State' onPress={clickHandler}/>
       </View>
       <View style={styles.body}>
         <Text> Hi this is a sample body text</Text>
@@ -36,5 +48,8 @@ const styles = StyleSheet.create({
     backgroundColor:'yellow',
     padding:20,
     fontWeight:'bold',
+  },
+  buttonContainer:{
+    marginTop:20,
   }
 });
