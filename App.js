993d0609705c2c20ec8a3f0385bcfe0050ba61b1
amantha6 <amantha@binghamton.edu>
2025-04-24 10:45:15 -0400

    Video 5 Text inputs

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const[name, setName]=useState('Aditi'); //name variable here will be given the value of Aditi
+  const[age,setAge]=useState('23');
   const[person,setPerson]=useState({name:'Aarushi', age:23});
   //in future if we want to update the value then we can use this function to do that
   //create a function here externally
@@ -15,7 +16,19 @@ export default function App() {
     <View style={styles.container}> 
       {/* can nest views within views */}
       <View style={styles.header}>
-        <Text style={styles.boldText}> My name is {name}</Text>
+        <Text> Enter Name: </Text>
+        <TextInput
+          multiline
+          style={styles.input}
+          placeholder='e.g. JohnDoe'
+          onChangeText={(val)=>setName(val)}/>
+        <Text> Enter age: </Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.input}
+          placeholder='e.g. 99'
+          onChangeText={(val)=>setAge(val)}/>
+        <Text style={styles.boldText}> My name is {name} and my age is {age}</Text>
         <Text> Her name is {person.name} and her age is {person.age}</Text>
       </View>
       <View style={styles.buttonContainer}>
@@ -51,5 +64,12 @@ const styles = StyleSheet.create({
   },
   buttonContainer:{
     marginTop:20,
+  },
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200
   }
 });
